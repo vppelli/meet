0a8d4909103df1e2d6875d602d89c9e49f646003
Mikey P <138417934+vppelli@users.noreply.github.com>
2024-01-23 13:54:31 -0500

    Updated api.js
    added localstorage for offline mode

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 // src/api.js
 
 import mockData from './mock-data';
+import NProgress from "nprogress";
 
 const getToken = async (code) => {
     const encodeCode = encodeURIComponent(code);
@@ -51,6 +52,12 @@ export const getEvents = async () => {
         return mockData;
     };
 
+    if (!navigator.onLine) {
+        const events = localStorage.getItem("lastEvents");
+        NProgress.done();
+        return events ? JSON.parse(events) : [];
+    }
+
     const token = await getAccessToken();
 
     if (token) {
@@ -59,6 +66,8 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
+            NProgress.done();
+            localStorage.setItem("lastEvents", JSON.stringify(result.events));
             return result.events;
         } else return null;
     };
