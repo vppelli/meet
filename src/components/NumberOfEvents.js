e9feda0ac36605238b163867eaa014c20bc45ea1
Mikey P <138417934+vppelli@users.noreply.github.com>
2024-01-24 14:38:08 -0500

    Added Lodash to dependences
    added a debounced to event numbers input

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,35 +1,29 @@
 // src/components/NumberOfEvents.js
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { debounce } from "lodash";
 
 const NumberOfEvents = ({ setCurrentNOE, setErrorAlert, setInfoAlert }) => {
     const [inputNumber, setInputNumber] = useState('32');
     const handleInputChanged = (event) => {
         const value = event.target.value;
-        if(isNaN(value) || value < 0){
+        if (isNaN(value) || value < 0) {
             setInputNumber(32);
             setCurrentNOE(32);
             setErrorAlert('Only positive numbers are allowed.');
             setInfoAlert('Defaulted back to 32 events');
-        }else{
+        } else {
             setInputNumber(value);
             setCurrentNOE(value);
             setErrorAlert('');
             setInfoAlert('');
         }
     };
-
-    useEffect(() => {
-        const delayDebounce = setTimeout(() => {
-            setInputNumber(inputNumber);
-        }, 500);
-        return () => clearTimeout(delayDebounce);
-    }, [inputNumber]);
-    // not sure if this works
+    const handleDebounce = debounce(handleInputChanged, 500);
 
     return (
         <div id="number-of-event">
-            <input type = "text" value = { inputNumber } onChange = { handleInputChanged } placeholder = "Type a Number"/>
+            <input type="text" onChange={handleDebounce} placeholder="Type a Number" defaultValue={inputNumber}/>
         </div>
     );
 }
