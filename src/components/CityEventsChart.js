650acde38708f1048cc309cf3157580a1bba3182
Mikey P <138417934+vppelli@users.noreply.github.com>
2024-01-26 16:55:35 -0500

    Debugging Charts

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -21,7 +21,7 @@ const CityEventsChart = ({ events, allLocations }) => {
 
     return (
         <ResponsiveContainer width="100%" height={400}>
-            <ScatterChart margin={{ top: 20, right: 20, bottom: 60, left: -30 }}>
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 60, left: 0 }}>
                 <CartesianGrid />
                 <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14, fill: "#fff"}} />
                 <YAxis type="number" dataKey="count" name="Number of events" tick={{fill: "#fff"}}/>
